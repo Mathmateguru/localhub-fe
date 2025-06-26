50b19a19d376ce81329f0a7a9b137c499bf1822f
hnobi <hnobi08@yahoo.com>
2025-06-26 14:29:26 +0100

    cleanup

--- src/pages/Signin/Signin.tsx
+++ src/pages/Signin/Signin.tsx
@@ -1,49 +1,46 @@
 import './signin.css'
 import { Users } from 'lucide-react'
- 
 
 
 
-const Signin = ()=>{
 
-    return(
-        <div className='main-wrapper'>
-             <div className='users-icon'>
-                 <Users size={40} />
-                 </div>
-                 <div className='Welcome'>
-                    <h1>Welcome Back</h1>
-                    </div>
-                   <span  className='signin-text'>
-                    <h6>Sign in to your Localhub account</h6>
-                    </span>
-                    
+const Signin = () => {
 
-                    <div className='user-detail'>
-                        <h3>Email</h3>
-                         <input type="Email" placeholder='Enter your email' className='signin-email'/>
-            
-                         
-                            <h3 className='password-header'>Password</h3>
-                            <input type="password" placeholder=' Enter your password' className='signin-password'/>
-                            
-            
-                          <button className='signin-btn'>Sign In</button>
-                            
-                            <div className='footer'>
-                            <p className='footer-txt1'>Don't have an account?</p>
-                            <p>Sign-In</p>
-                            </div>
+  return (
+    <div className='main-wrapper'>
+      <form className='signin_form'>
+        <div className="font">
+          <Users size={50} />
+        </div>
+        <h2 className="signup_header">Welcome Back</h2>
+        <p className="signup_sub_header">Sign in to your Localhub account</p>
+        <div>
+          <div>
+            <label htmlFor='email'>Email</label>
+            <input id='email' type="text" placeholder="abcd@gmail.com" className="form_input" />
+          </div>
 
 
-                         </div>
+          <h3 className='password-header'>Password</h3>
+          <input type="password" placeholder=' Enter your password' className='form_input' />
 
-                    </div>
+
+          <button className='signin-btn'>Sign In</button>
+
+          <div className='footer'>
+            <p className='footer-txt1'>Don't have an account?</p>
+            <p>Sign-In</p>
+          </div>
+
+
+        </div>
+      </form>
+    </div>
 
 
 
-        
-    )
+
+  )
 
 
 }
