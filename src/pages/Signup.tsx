8b7ed8e15b0937f3e6881b25a560ed1a22b8364b
hnobi <hnobi08@yahoo.com>
2025-07-01 11:03:59 +0100

    integrate signup api

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,7 +1,38 @@
-import { Users, Eye } from 'lucide-react';
+import { Users } from 'lucide-react';
 import "./Signup.css";
+import { useState } from 'react';
+import axios from 'axios';
+  import { toast } from 'react-toastify';
+
+const baseURL = import.meta.env.VITE_API_URL;
+
 
 const Signup = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+ 
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const data = { name, email, password }
+    setLoading(true);
+    try {
+      const res = await axios.post(`${baseURL}/signup`, data);
+      setLoading(false);
+      if (res.data.status === 'success') {
+        toast(res.data.message, { type: 'success' });
+      } 
+    } catch (error: any) {
+       setLoading(false);
+      if(error?.response.data.status === 'error') {
+       toast(error?.response.data.message, {type: 'error'});
+       
+      }
+    }
+  }
+
   return (
     <div className="signup_page">
       <div className="signup_form">
@@ -13,18 +44,35 @@ const Signup = () => {
         <div >
           <div>
             <label htmlFor='name'>Name</label>
-            <input id='name' type="text" placeholder="Enter your full name" className="form_input" />
+            <input
+              onChange={(e) => setName(e.target.value)}
+              id='name'
+              type="text"
+              placeholder="Enter your full name"
+              className="form_input" />
           </div>
 
           <h2>Email</h2>
-          <input type="text" placeholder="Enter your email" className="form_input" />
+          <input
+            type="text"
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your email"
+            className="form_input" />
           <div>
             <label htmlFor='password'>Password</label>
-            <input id='password' type="password" className="form_input" />
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              id='password'
+              type="password"
+              className="form_input" />
             {/* <Eye size={20} className="eye_icon" /> */}
           </div>
           <div>
-            <button className="signup_button">Create Account</button>
+            <button 
+            disabled={loading}
+            onClick={handleSubmit} className="signup_button">
+             {loading ? 'Loading...' : 'Sign Up'}
+              </button>
           </div>
           <div className="wrapper_bottom_text">
             <p className="bottom_text_0ne">Already have an account?</p>
